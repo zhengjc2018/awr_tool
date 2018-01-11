1bd71a1760061c0b6071b31821b4187641ad57aa
zhengjc2018 <35327570+zhengjc2018@users.noreply.github.com>
2018-01-11 16:36:00 +0800

    Add files via upload

---
+++ init.py
@@ -0,0 +1,17 @@
+init_para_cmplist={
+"sec_case_sensitive_logon":"FALSE",
+"deferred_segment_creation":"FALSE",
+"audit_trail":"NONE",
+"_serial_direct_read":"NEVER",
+"pre_page_sga":"FALSE",
+"_use_adaptive_log_file_sync":"FALSE",
+"_optimizer_cost_based_transformation":"OFF",
+"_external_scn_rejection_threshold_hours":"1",
+"_external_scn_logging_threshold_seconds":"600",
+"event":"28401 TRACE NAME CONTEXT FOREVER, LEVEL 1",
+"parallel_force_local":"TRUE",
+"fast_start_parallel_rollback":"HIGH",
+"_gc_read_mostly_locking":"FALSE",
+"_gc_policy_time":"0",
+"_clusterwide_global_transactions":"FALSE"
+}
